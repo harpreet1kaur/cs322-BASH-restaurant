a648d946d21b7fa2d42ba3f68b52012cf02ce999
harpreet1kaur <123113476+harpreet1kaur@users.noreply.github.com>
2024-04-24 17:41:24 -0700

    Update chefClass.py
    possible additions to the chef class?

--- src/utils/roles/chefClass.py
+++ src/utils/roles/chefClass.py
@@ -12,3 +12,31 @@ class ChefClass(BasicClass):
         print(f"{self.name} is serving {dish}.")
 
 
+# Import OrderManagement and MenuManagement classes
+from orderManagement import OrderManagement
+from menuManagement import MenuManagement
+
+# Define the Employee class
+class Employee:
+    def __init__(self, employee_id, first_name, last_name, password, username):
+        self.employee_id = employee_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.username = username
+
+# Define the Chef class inheriting from Employee
+class Chef(Employee):
+    def __init__(self, employee_id, first_name, last_name, password, username, specialty):
+        super().__init__(employee_id, first_name, last_name, password, username)
+        self.specialty = specialty
+
+    def changeItemStatus(self, item_id, status):
+        # Access MenuManagement to change the status of a menu item
+        menu_mgmt = MenuManagement()
+        menu_mgmt.change_item_status(item_id, status)
+
+    def setOrderStatus(self, order_id, status):
+        # Access OrderManagement to set the status of an order
+        order_mgmt = OrderManagement()
+        order_mgmt.set_order_status(order_id, status)
